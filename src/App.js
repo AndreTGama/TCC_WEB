2d00d4bf6ba8f1c895530882d923a0a557840833
Andre Gama <andre_gama789@hotmail.com>
2020-08-31 19:49:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import env from './env';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter basename={env.BASE_NAME}>
+			<Routes />
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { useHistory, Link } from "react-router-dom";
+import api from "../../services/api";
+import "./style.css";
+
+export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorLogin, setErrorLogin] = useState("");
+	const [disableButton, setDisableButton] = useState(false);
+	const [buttonLogin, setButtonLogin] = useState("Entrar");
+	const history = useHistory();
+
+	function handleLogin(event) {
+		setButtonLogin("Carregando...");
+		setDisableButton(true);
+		event.preventDefault();
+		setErrorLogin("");
+		(async function() {
+			const response = await api.post("/login", {
+				login: email,
+				password: password,
+			});
+			console.log(response);
+			if (response.data.error === false) {
+				history.push("/usuario_admin");
+				alert(response.data.data);
+			} else {
+				alert(`${response.data.message}`);
+				setErrorLogin(response.data.message);
+			}
+			setButtonLogin("Entrar");
+			setDisableButton(false);
+		})();
+	}
+
+	return (
+		<section className="background_page_form">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-12 col-lg-7 col-md-9">
+						<div className="card" id="login-card">
+							<div className="card-body">
+								<form
+									className="login_page_form"
+									onSubmit={handleLogin}
+								>
+									<div className="row justify-content-center text-center">
+										<div className="form-group col-10">
+											<label htmlFor="loginEmail">
+												<b>Login</b>
+											</label>
+											<input
+												type="text"
+												name="loginEmail"
+												id="loginEmail"
+												placeholder="Digite seu login"
+												className="form-control"
+												onChange={(event) =>
+													setEmail(event.target.value)
+												}
+												required
+											/>
+										</div>
+									</div>
+									<div className="row justify-content-center text-center">
+										<div className="form-group col-10">
+											<label htmlFor="passwordLogin">
+												<b>Senha</b>
+											</label>
+											<input
+												type="password"
+												name="passwordLogin"
+												id="passwordLogin"
+												placeholder="Digite a sua senha"
+												className="form-control"
+												onChange={(event) =>
+													setPassword(
+														event.target.value
+													)
+												}
+												required
+											/>
+										</div>
+									</div>
+									<div className="row justify-content-center">
+										<div className="form-group col-10">
+											<span className="errorLogin">
+												{errorLogin}
+											</span>
+										</div>
+									</div>
+									<div className="row justify-content-center text-center">
+										<div className="col-10 form-group">
+											<button
+												type="submit"
+												disabled={disableButton}
+												className="btn btn-primary btn-block"
+												id="loginBtn"
+											>
+												{buttonLogin}
+											</button>
+										</div>
+									</div>
+									<div className="row justify-content-center text-center">
+										<div className="col-10 form-group">
+											<Link
+												to={"/esqueceu-senha"}
+												id="forgotpassword"
+											>
+												Esqueceu Senha?
+											</Link>
+										</div>
+									</div>
+									<div className="row justify-content-center text-center">
+										<div className="col-10 form-group">
+											<Link
+												to={"/registro"}
+												id="forgotpassword"
+											>
+												Cadastro
+											</Link>
+										</div>
+									</div>
+								</form>
+								<div className="row justify-content-center text-center">
+									<div className="col-10 form-group"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
