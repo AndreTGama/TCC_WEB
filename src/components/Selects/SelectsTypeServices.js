d36d8ed34765c4628c982a9221f757286b46d919
Andre Gama <andre_gama789@hotmail.com>
2020-11-01 15:27:47 -0300

    selects feitos

---
+++ src/components/Selects/SelectsTypeServices.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+// eslint-disable-next-line react/prop-types
+export default function SelectsTypeServices({ setIdTypeService, valueSelect, disabled }) {
+	const [TypeServices, setTypeServices] = useState([]);
+
+	useEffect(() => {
+		(async function() {
+			const response = await api.get('/selects/type-services');
+			setTypeServices(response.data.data);
+		})();
+	}, []);
+
+	// eslint-disable-next-line no-shadow
+	async function handleSelect(id) {
+		setIdTypeService(id);
+	}
+	const TypeServicesList = TypeServices.map(t => (
+		<option value={t.id_type_service} key={t.id_type_service}>
+			{t.type_service}
+		</option>
+	));
+	return (
+		<>
+			<select
+				disabled={disabled}
+				required="required"
+				className="form-control"
+				name="IdTipoServico"
+				id="IdTipoServico"
+				value={valueSelect}
+				onChange={event => handleSelect(event.target.value)}
+			>
+				<option value="">Seleciona um tipo de serviço</option>
+				{TypeServicesList}
+			</select>
+		</>
+	);
+}
