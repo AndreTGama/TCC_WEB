35c237b83cab1ff382c58b2eb4ea1063d6a7d28b
Andre Gama <andre_gama789@hotmail.com>
2020-11-08 19:36:59 -0300

    removendo o styles

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -40,7 +40,7 @@ export default function Profile() {
 
 	return (
 		<>
-			<div class="card" style="width: 18rem;">
+			<div class="card">
 				<div class="card-body">
 					<h5 class="card-title">Card title</h5>
 					<p class="card-text">
